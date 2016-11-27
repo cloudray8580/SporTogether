41dd971ffd37b27d232f6db3b6b3c6c926074b18
cloudray8580 <240078316@qq.com>
2016-11-27 10:58:59 +0800

    add edit info activity

---
+++ app/src/main/java/com/cloudray/sportogether/view/activity/EditPersonalInfoActivity.java
@@ -0,0 +1,15 @@
+package com.cloudray.sportogether.view.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.cloudray.sportogether.R;
+
+public class EditPersonalInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_personal_info);
+    }
+}
